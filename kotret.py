7fec705d42340e4e8bf5adfa108f4b807f803225
FutuhIqramMultazam <futuhiqramm@gmail.com>
2024-08-05 17:42:08 +0700

    first

---
+++ kotret.py
@@ -0,0 +1,10 @@
+nama = input("masukan nama anda:")
+
+if nama == "icam":
+    kabar = input("hai icam senang bertemu dengan mu, bagaimana kabar mu?")
+    if(kabar == "baik"):
+        print("alhamdulilah")
+    else:
+        print("syafakallah icam")
+else:
+    print("siapa kamu, saya tidak mengenal mu")
